02ad1dc07ac5bdce4087575744a6ca950a56c7db
Icontol <119477979+Icontol@users.noreply.github.com>
2023-01-06 22:11:49 +0700

    Create payment.js

---
+++ plugins/payment.js
@@ -0,0 +1,36 @@
+import fetch from 'node-fetch'
+
+//Plugin By Xynoz!!
+let handler = async (m, { conn, usedPrefix }) => {
+  const ultah = new Date('September 10 2023 00:00:01')
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
+  let vn = `https://github.com/Icontol/kontolbapakkaupecah/blob/master/mp3/ivan.mp3?raw=true`
+  conn.sendButton(m.chat, `
+┏━ꕥ〔 *Donasi • Dana* 〕ꕥ━⬣
+┃✾ Gopay  [6285876830674]
+┃✾ Dana  [6285876830674]
+┗━━━━ꕥ
+┏━━ꕥ〔 *NOTE* 〕ꕥ━⬣
+┃ Ingin donasi? Wa.me/6285876830674
+┃ _Hasil donasi akan digunakan buat sewa_
+┃ _atau beli *RDP/VPS* agar bot bisa jalan_
+┃ _24jam tanpa kendala_
+┗━━━━ꕥ
+`.trim(), wm, [['Owner',`/owner`]],m) // Tambah sendiri kalo mau
+conn.sendFile(m.chat, vn, 'haori.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true, contextInfo:{ externalAdReply: { title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`, body: `Join yuk`, sourceUrl: 'https://chat.whatsapp.com/EPmjuw9X1wZDGX1p4NCV1e', thumbnail: await (await fetch('https://ibb.co/3R2fQCT')).buffer(),}} 
+     }) 
+}
+handler.command = /^(pay|payment|bayar)$/i
+handler.tags = ['info']
+handler.help = ['payment']
+handler.premium = false
+handler.limit = true
+
+export default handler
