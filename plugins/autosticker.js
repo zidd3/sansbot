5d1cc8e246e555c4e82bb6c128423d07d91433ae
Icontol <119477979+Icontol@users.noreply.github.com>
2023-01-11 22:17:48 +0700

    Update autosticker.js

--- plugins/autosticker.js
+++ plugins/autosticker.js
@@ -21,7 +21,7 @@ let stikerhuuu = [
  "https://telegra.ph/file/10f1e8b0ac5322767dae8.jpg",//anime yntkts
  "https://telegra.ph/file/c2bee8248ed3798f7f293.jpg",//windah bocil
  "https://telegra.ph/file/41ca2ab35c54bded789c1.jpg",//patrick bawa minum
- "https://telegra.ph/file/59410d72141a46bf8dc5d.jpg",//pak polisi pap tt
+ "https://telegra.ph/file/59410d72141a46bf8dc5d.jpg",//pak polpisi pap tt
  "https://telegra.ph/file/c807055f8d88abc468536.jpg",//kucing1
  "https://telegra.ph/file/b919a037a4d333bdf61c9.jpg",//kacamata
 ]
